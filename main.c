99fce5b71e154109d2283aac232f9590f28dac46
rai8 <hytonne@outlook.com>
2022-05-18 10:07:17 +0300

    simple shell com

--- main.c
+++ main.c
@@ -1,49 +1,38 @@
+#include "general.h"
 #include "shell.h"
-/**
- * main - This is a simple shell created by
- * for ALX  School
- * Return: 0 if success
- */
-int main(void)
-{
-	ssize_t bytes_rd = 0; /** Bytes read from a getline*/
-	size_t bf_size = 0; /**Buffer size*/
-	char *entry = NULL, *arguments[20]; /**String of args that enters the usr*/
-	int counter = 1, vf_stat = 0, exist_stat = 0, exit_stat = 0, blt_stat = 0;
 
-	_printp("$ ", 2);/**prompt mini-shell*/
-	bytes_rd = getline(&entry, &bf_size, stdin); /**sizeof entry, o -1 (EOF))*/
-	while (bytes_rd != -1)
+/**
+ * main - Entry point of the shell
+ *
+ * @argc: Number of arguments received
+ * @argv: Arguments received
+ *
+ * Return: 0 on success and 1 on error
+ **/
+int main(int argc, char **argv)
+{
+	general_t *info;
+	int status_code;
+
+	info = malloc(sizeof(general_t));
+	if (info == NULL)
 	{
-		if (*entry != '\n')
-		{
-			fill_args(entry, arguments);
-			if (arguments[0] != NULL)
-			{
-				exist_stat = exist(arguments[0]);/**Exist evaluates if the path entered exists*/
-				if (exist_stat != 0)/**Did not find the file*/
-				{
-					vf_stat = verify_path(arguments);
-					if (vf_stat == 0)
-						exit_stat = exec(arguments), free(entry), free(*arguments);
-					else
-					{
-					blt_stat = verify_blt(arguments, exit_stat);
-					if (blt_stat != 0)
-						exit_stat = print_not_found(arguments, counter), free(entry);
-					}
-				}
-				else /**Found the file*/
-					exit_stat = exec(arguments), free(entry);
-			}
-			else
-				free(entry);
-		}
-		else if (*entry == '\n')
-			free(entry);
-		entry = NULL, counter++;
-		_printp("$ ", 2), bytes_rd = getline(&entry, &bf_size, stdin);
+		perror(argv[0]);
+		exit(1);
 	}
-	last_free(entry);
-	return (exit_stat);
+
+	info->pid = getpid();
+	info->status_code = 0;
+	info->n_commands = 0;
+	info->argc = argc;
+	info->argv = argv;
+	info->mode = isatty(STDIN) == INTERACTIVE;
+	start(info);
+
+	status_code = info->status_code;
+
+	free(info);
+
+	return (status_code);
 }
+
